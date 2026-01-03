47ffa1702bcceb2a8a4414b24123e81a867e41c3
Kei Zimmermann <zimamankei@gmail.com>
2026-01-03 14:59:03 +0200

    fixed webapps text

--- components/services/webapps/header.tsx
+++ components/services/webapps/header.tsx
@@ -2,7 +2,7 @@ export default function WebAppsHeader() {
   return (
     <div className="flex h-96 w-full items-center justify-center">
       <p className="font-sans text-sm font-medium text-gray-800">
-        This page is undergoing maintenance
+        This page is undergoing maintenance. Check back soon.
       </p>
     </div>
   );
