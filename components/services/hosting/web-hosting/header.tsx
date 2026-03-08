cee196ca7a6562f77213b79ec38a4267429c6c91
Kei Zimmermann <zimamankei@gmail.com>
2026-03-08 15:00:55 +0200

    finished adding blog

--- components/services/hosting/web-hosting/header.tsx
+++ components/services/hosting/web-hosting/header.tsx
@@ -6,7 +6,7 @@ import { IoShieldCheckmarkSharp } from "react-icons/io5";
 import { BsLightningChargeFill } from "react-icons/bs";
 import { FaServer } from "react-icons/fa6";
 import Image from "next/image";
-import { greenButton, pageHeadings } from "@/app/ui/customTailwindClasses";
+import { greenButton } from "@/app/ui/customTailwindClasses";
 import Link from "next/link";
 import { useCurrency } from "@/contexts/currency-context";
 
